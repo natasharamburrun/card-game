92ab386afa653995d19ccc7b16bfb3837994fa9a
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-11-05 14:08:19 +0000

    changed to es6 syntax

--- src/app.js
+++ src/app.js
@@ -1,43 +1,79 @@
-var cards = [];
+window.onload = () => {
+  const message = document.getElementById('message');
+  const start = document.getElementById('start');
 
+  let cards = [];
+  const suits = ['spades','hearts','clubs','diams'];
+  const numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
-var suits = ['spades','hearts','clubs','diams'];
-var numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+  const cardOutput = document.getElementById('cards');
 
-function buildCards(){
-  cards = [];
-  for(s in suits){
-    var suit = suits[s][0].toUpperCase();
-    for(n in numbers){
-      var card = {
-        suit: suits[s],
-        num: numbers[n],
-        cardValue: parseInt(n) + 1,
-        icon: suit
-      };
-      cards.push(card);
+  const count = 0;
+
+  start.onclick = () => {
+    message.innerHTML = 'Welcome to the game!';
+    document.getElementById('start').style.display= 'none';
+    document.getElementById('addCard').style.display= 'none';
+    buildDeck();
+    playerTwo();
+    playerOne();
+  };
+
+  const buildDeck = () => {
+    cards = [];
+    for(s in suits){
+      const suit = suits[s][0].toUpperCase();
+      for(n in numbers){
+        const card = {
+          suit: suits[s],
+          num: numbers[n],
+          cardValue: parseInt(n) + 1,
+          icon: suit
+        };
+        cards.push(card);
+      }
     }
-  }
-  console.log(cards);
-}
+    console.log(cards);
+  };
 
-function incrementCards(a){
-  count++;
-  cardOutput.innerHTML += showCard();
-}
+  const shuffleArrays = (array) => {
+    for(var i = array.length -1; i > 0; i--){
+      var holder = Math.floor(Math.random() * (i + 1));
+      // console.log(array[holder]);
+      var temp = array[i];
+      array[i] = array[holder];
+      array[holder] = temp;
+    }
+    return array;
+  };
+
+  const showCard = () => {
+    var c = cards[count];
+    var bgColor = (c.icon === 'H' || c.icon === 'D')?'red':'black';
+    return '<span class="cCard "style="color:'+bgColor+'">' + c.num + '&' + c.suit + ';</span>';
+  };
+
+  const playerTwo = () => {
+    shuffleArrays(cards);
+    showCard();
+    cardOutput.innerHTML += showCard();
+  };
+
+  const playerOne = () => {
+    shuffleArrays(cards);
+    showCard();
+    cardOutput.innerHTML += showCard();
+  };
+};
+
+// function incrementCards(a){
+//   count++;
+//   cardOutput.innerHTML += showCard();
+// }
+//
+//
 
 // function winGame(){
 //   var win = false;
 //   if()
 // }
-
-function shuffleArrays(array){
-  for(var i = array.length -1; i > 0; i--){
-    var holder = Math.floor(Math.random() * (i + 1));
-    // console.log(array[holder]);
-    var temp = array[i];
-    array[i] = array[holder];
-    array[holder] = temp;
-  }
-  return array;
-}
